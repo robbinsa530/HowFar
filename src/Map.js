08382c2c48af49554353dbe1fb6e0b4c751cc5d0
robbinsa530 <arobbins530@hotmail.com>
2024-04-07 22:01:51 -0600

    Add ability to click and place markers

--- src/Map.js
+++ src/Map.js
@@ -4,13 +4,14 @@ import './Map.css';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXJvYmJpbnM1MzAiLCJhIjoiY2x1b2xzOGNtMXhpMTJrbGQ2YnA0bDlhcSJ9.jyhalVCpoVufi45_mrB-zw';
 
+let markers = [];
 
 function Map() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const [lng, setLng] = useState(-104.9);
+  const [lat, setLat] = useState(39.8);
+  const [zoom, setZoom] = useState(12);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
@@ -24,6 +25,10 @@ function Map() {
     // Default cursor should be pointer
     map.current.getCanvas().style.cursor = 'crosshair';
 
+    // Add zoom control and geolocate
+    map.current.addControl(new mapboxgl.NavigationControl(), "bottom-right");
+    map.current.addControl(new mapboxgl.GeolocateControl({showAccuracyCircle: false, showUserLocation: false}));
+
     // For displaying current location
     map.current.on('move', () => {
       setLng(map.current.getCenter().lng.toFixed(4));
@@ -33,18 +38,22 @@ function Map() {
 
     // Place a marker on click
     map.current.on('click', e => {
-      // Create a new DOM node and save it to a React ref
-      const ref = React.createRef();
-      ref.current = document.createElement('div');
-    
-      // Create a Mapbox Marker at our new DOM node
-      new mapboxgl.Marker({
-        className: "marker", 
-        element: ref.current,
-        draggable: true
-      })
-        .setLngLat(e.lngLat)
-        .addTo(map.current);
+      // Only add marker if click was NOT on an existing marker
+      if (!markers.includes(e.originalEvent.target)) {
+        // Create a new DOM node and save it to a React ref
+        const ref = React.createRef();
+        ref.current = document.createElement('div');
+        markers.push(ref.current);
+      
+        // Create a Mapbox Marker at our new DOM node
+        new mapboxgl.Marker({
+          className: "marker",
+          element: ref.current,
+          draggable: true
+        }).setLngLat(e.lngLat)
+          .setPopup(new mapboxgl.Popup().setText("TODO: Make this do something"))
+          .addTo(map.current);
+      }
 
       // Clean up on unmount
       return () => map.remove();
